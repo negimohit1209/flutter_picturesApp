c703c549369bdbc2fea86df11bacf89d894df15a
negimohit1209 <mohitsngh1997@gmail.com>
2019-07-17 11:09:03 +0530

    add model

--- lib/App/app.dart
+++ lib/App/app.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   _AppState createState() => _AppState();
@@ -6,18 +7,20 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   var _counter = 0;
+
+  void fetchImage() {
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _counter += 1;
-          });
-        },
+        onPressed: fetchImage,
         child: Icon(Icons.add),
       ),
-      body: Center(child: Text("$_counter"),),
+      body: Center(
+        child: Text("$_counter"),
+      ),
       appBar: AppBar(
         title: Text('Mohit'),
       ),
