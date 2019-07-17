9a43b7667b3eda0468fb2cd3287f3556fbcf7662
negimohit1209 <mohitsngh1997@gmail.com>
2019-07-17 13:16:45 +0530

    add asynchronous code and used future

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -4,16 +4,32 @@ import '../model/image_model.dart';
 class ImageList extends StatelessWidget {
   final List<ImageModel> images;
 
-  
   ImageList(this.images);
 
   @override
   Widget build(BuildContext context) {
+    Widget buildImage(ImageModel image) {
+      return Container(
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+        ),
+        child: Column(
+          children: <Widget>[
+            Image.network(image.url),
+            SizedBox(height: 10,),
+            Text(image.title),
+          ],
+        ),
+      );
+    }
+
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (BuildContext context, int index) {
-      return Text(images[index].url);
-     },
+        return buildImage(images[index]);
+      },
     );
   }
 }
